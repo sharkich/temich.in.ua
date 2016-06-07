c1c986813ae548db7d4b93a02105f8fdcf4940e1
Artem Koziar <a@temich.in.ua>
2016-06-07 23:48:18 +0300

    first commands

--- src/app/_services/commands/commands.service.ts
+++ src/app/_services/commands/commands.service.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import { Router } from '@angular/router-deprecated';
 import {HmrState} from 'angular2-hmr';
 
 @Injectable()
@@ -6,12 +7,45 @@ export class Commands {
   // @HmrState() is used by HMR to track the state of any object during a hot module replacement
   @HmrState() _logs = [];
 
-  constructor() {
+  constructor(private router:Router) {
   }
 
-  run(command:string) {
-    this._logs.push(command);
+  runJS(command: string) {
+    let result;
+    try {
+      result = eval(command);
+    } catch(err) {
+      console.log(err);
+    }
+    return result;
+  }
+
+  run(command: string) {
+    let result;
+
+    command = command.trim().toLocaleLowerCase();
+
+    result = command ? this.runJS(command) : '';
+
+    this._logs.push({
+      command,
+      result
+    });
     console.log(`@Commands.run('${command}')`, this._logs);
+
+    if (result !== undefined || !command) {
+      return;
+    }
+
+    switch(command) {
+      case 'cd /':
+        this.router.navigate(['/Home']);
+        break;
+      case 'cd /about':
+        this.router.navigate(['/About']);
+        break;
+    }
+
   }
 
 }
