95476904f00991e5de2d146f96e55a9e87de6359
Temich Koziar <a@temich.in.ua>
2017-10-31 23:47:20 +0200

    [v4] angular-cli init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,10 @@
-/**
- * Angular 2 decorators and services
- */
-import {
-  Component,
-  OnInit,
-  ViewEncapsulation
-} from '@angular/core';
-import { AppState } from './app.service';
+import { Component } from '@angular/core';
 
-/**
- * App Component
- * Top Level Component
- */
 @Component({
-  selector: 'app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  encapsulation: ViewEncapsulation.None,
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  constructor(public appState: AppState) {}
-
-  public ngOnInit() {
-    console.log('Initial App State', this.appState.state);
-  }
-
+export class AppComponent {
+  title = 'app';
 }
