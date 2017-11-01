2e37487bea5f247017478ed08ab5a20d9786f819
Temich Koziar <a@temich.in.ua>
2017-11-01 02:55:40 +0200

    [v4] menu item hover

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +8,35 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  public AVATARS = {
+    '/': '/assets/img/avatars/hololens.jpg',
+    '/family': '/assets/img/avatars/family.jpg',
+    '/log': '/assets/img/avatars/hard-working.jpg',
+    '/cv': '/assets/img/avatars/wc.jpg',
+    '/contacts': '/assets/img/avatars/phone.jpg'
+  };
+
+  public avatarSRC: string = this.AVATARS['/'];
+
+  constructor(private router: Router) {
+    console.log(router);
   }
 
   ngOnInit() {
+  }
+
+  onMenuItemHover (avatarSrc: string) {
+    this.avatarSRC = avatarSrc;
+  }
+
+  onMenuItemOver () {
+    let findKey = Object.keys(this.AVATARS).find((key) => {
+      return key !== '/' && this.router.url.indexOf(key) !== -1;
+    });
+    if (!findKey) {
+      findKey = '/';
+    }
+    this.avatarSRC = this.AVATARS[findKey];
   }
 
 }
